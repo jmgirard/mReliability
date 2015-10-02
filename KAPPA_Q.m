19dbadaaf26780a6665f98b50943c065c2e79188
jmgirard <j.girard@pitt.edu>
2015-10-02 16:47:10 -0400

    Initial commit

--- KAPPA_Q.m
+++ KAPPA_Q.m
@@ -1,13 +1,12 @@
-function [AC, p_a, p_e, SE, CI, IMP, BM] = GWETAC(M, type, f)
-% Calculate Gwet's Agreement Coefficient (AC1/AC2)
-%   [AC, p_a, p_e, SE, CI, IMP, BM] = GWETAC(M, type, f)
+function [KAPPA, P_A, P_E, SE, CI, IMP, BM] = KAPPA_Q(DATA, TYPE, RATIO)
+% Calculate Kappa Q or the Free Marginal Agreement Coefficient
+%   [KAPPA, P_A, P_E, SE, CI, IMP, BM] = KAPPA_Q(DATA, TYPE, RATIO)
 %
-%	M is a numerical matrix of measurements. Each row is an object of
-%	measurement and each column is a single source of measurement.
-%	Measurements should be made numerical (e.g., A=>1, B=>2, C=>3).
-%	Missing measurements should be indicated by NaN, Inf, or -Inf.
+%	DATA is a numerical matrix of codes with missing data indicated by NaN.
+%   Each row is a single item and each column is a single coder.
+%	Codes should be made numerical (e.g., APPLE=>1, ORANGE=>2, BANANA=>3).
 %
-%   type is a string corresponding to the type of weights to be used.
+%   TYPE is a string corresponding to the type of weights to be used.
 %   'nominal' weights are used for unordered categories
 %   'ordinal' weights are used for ordered categories of unequal size
 %   'linear' weights are used for ordered categories with equal spacing
@@ -15,23 +14,22 @@ function [AC, p_a, p_e, SE, CI, IMP, BM] = GWETAC(M, type, f)
 %   'radical' weights are used for ordered categories with equal spacing
 %   'ratio' weights are used for ordered categories with a meaningful zero
 %
-%	f is the sampling fraction for the current reliability experiment.
-%	To generalize from a sample of n objects to a population of N objects,
-%	set f equal to the fraction of n to N (i.e., f = n / N). If the size
-%	of the population of objects is unknown, then use the default of 0.
+%	RATIO is the sampling fraction for the current reliability experiment.
+%	To generalize from a sample of n items to a population of N items, set
+%   RATIO to the fraction of n to N (i.e., n/N). If the population size is
+%   unknown, then use the default of 0. This parameter is optional.
 %
-%	AC is an index of the agreement between two or more sources of
-%	measurement that corrects for agreement due to chance.
-%	It is more "paradox-resistant" than most other agreement indices.
+%	KAPPA is a chance-corrected agreement index that estimates chance
+%	agreement using a category-based approach.
 %
-%	p_a is the percent agreement observed between sources of measurement.
+%	P_A is the percent agreement observed between coders.
 %
-%	p_e is the percent agreement expected to be due to random guessing. 
+%	P_E is the percent agreement expected to be due to chance. 
 %   
-%	SE is the standard error of the AC estimate (conditional on rater sample).
+%	SE is the standard error of the estimate (conditional on rater sample).
 %
 %	CI is a two-element vector containing the lower and upper bounds of
-%	the 95% confidence interval for the AC estimate.
+%	the 95% confidence interval for the K estimate.
 %
 %	IMP is the interval membership probability for Altman's benchmarks:
 %	'Very Good':	0.8 to 1.0
@@ -45,15 +43,15 @@ function [AC, p_a, p_e, SE, CI, IMP, BM] = GWETAC(M, type, f)
 %
 %   (c) Jeffrey M Girard, 2015
 %   
-%   Reference: Gwet, K. L. (2014). Handbook of inter-rater reliability:
-%       The definitive guide to measuring the extent of agreement
-%       among raters (4th ed.). Gaithersburg, MD: Advanced Analytics.
+%   Reference: Brennan, R. L., & Prediger, D. J. (1981).
+%   Coefficient Kappa: Some uses, misuses, and alternatives.
+%   Educational and Psychological Measurement, 41(3), 687–699.
 
 %% Calculate variables
-if nargin < 3, f = 0; end
-M(all(~isfinite(M),2),:) = [];
-[n,~] = size(M);
-x = unique(M(:));
+if nargin < 3, RATIO = 0; end
+DATA(all(~isfinite(DATA),2),:) = [];
+[n,~] = size(DATA);
+x = unique(DATA(:));
 x(~isfinite(x)) = [];
 q = length(x);
 
@@ -61,7 +59,7 @@ q = length(x);
 w = nan(q,q);
 for k = 1:q
     for l = 1:q
-        switch type
+        switch TYPE
             case 'nominal'
                 w = eye(q);
             case 'ordinal'
@@ -101,51 +99,35 @@ for k = 1:q
     end
 end
 
-%% Calculate pi for each category
-pi_k = zeros(q,1);
-for k = 1:q
-    for i = 1:n
-        r_ik = sum(M(i,:)==x(k));
-        r_i = sum(isfinite(M(i,:)));
-        pi_k(k) = pi_k(k) + r_ik / r_i;
-    end
-    pi_k(k) = pi_k(k) / n;
-end
-
 %% Calculate percent agreement for each item and overall
 p_a_i = zeros(n,1);
 for i = 1:n
-    r_i = sum(isfinite(M(i,:)));
+    r_i = sum(isfinite(DATA(i,:)));
     if r_i >= 2
         for k = 1:q
-            r_ik = sum(M(i,:)==x(k));
+            r_ik = sum(DATA(i,:)==x(k));
             rstar_ik = 0;
             for l = 1:q
                 w_kl = w(k,l);
-                r_il = sum(M(i,:)==x(l));
+                r_il = sum(DATA(i,:)==x(l));
                 rstar_ik = rstar_ik + (w_kl * r_il);
             end
             p_a_i(i) = p_a_i(i) + (r_ik * (rstar_ik - 1)) / (r_i * (r_i - 1));
         end
     end
 end
-p_a = sum(p_a_i) / sum(sum(isfinite(M),2)>=2);
+P_A = sum(p_a_i) / sum(sum(isfinite(DATA),2)>=2);
 
 %% Calculate percent chance agreement for each item and overall
 p_e_i = zeros(n,1);
 for i = 1:n
-    r_i = sum(isfinite(M(i,:)));
-    for k = 1:q
-        r_ik = sum(M(i,:)==x(k));
-        p_e_i(i) = p_e_i(i) + (r_ik / r_i) * (1 - pi_k(k));
-    end
     T_w = sum(sum(w));
-    p_e_i(i) = p_e_i(i) * (T_w / (q * (q - 1)));
+    p_e_i(i) = T_w / (q ^ 2);
 end
-p_e = mean(p_e_i);
+P_E = mean(p_e_i);
 
-%% Calculate AC point estimate
-AC = (p_a - p_e) / (1 - p_e);
+%% Calculate K point estimate
+KAPPA = (P_A - P_E) / (1 - P_E);
 
 %% Return if variance is not requested
 if nargout <=3
@@ -153,18 +135,24 @@ if nargout <=3
     return;
 end
 
-%% Calculate the variance of the AC point estimate
-v_inner = nan(n,1);
+%% Calculate variance of K point estimate
+K_i = nan(n,1);
+v_inner = 0;
 for i = 1:n
-    gamma_i = (p_a_i(i) - p_e) / (1 - p_e);
-    gammastar_i = gamma_i - 2 * (1 - AC) * ((p_e_i(i) - p_e) / (1 - p_e));
-    v_inner(i) = (gammastar_i - AC) ^ 2;
+    r_i = sum(isfinite(DATA(i,:)));
+    if r_i >= 2
+        nprime = sum(~isnan(p_a_i));
+        K_i(i) = (n / nprime) * (p_a_i(i) - P_E) / (1 - P_E);
+    else
+        K_i(i) = 0;
+    end
+    v_inner = v_inner + (K_i(i) - KAPPA) ^ 2;
 end
-v = ((1 - f) / n) * (1 / (n - 1)) * sum(v_inner);
+v = ((1 - RATIO) / n) * (1 / (n - 1)) * sum(v_inner);
 
 %% Calculate the standard error and confidence interval
 SE = sqrt(v);
-CI = [AC - 1.96 * SE, AC + 1.96 * SE];
+CI = [KAPPA - 1.96 * SE, KAPPA + 1.96 * SE];
 
 %% Calculate the interval membership probabilities
 intervals = [0.8, 1.0; 0.6, 0.8; 0.4, 0.6; 0.2, 0.4; -1, 0.2];
@@ -172,7 +160,7 @@ IMP = nan(1,size(intervals,1));
 for z = 1:size(intervals,1)
     a = intervals(z,1);
     b = intervals(z,2);
-    IMP(z) = normcdf((AC - a) / SE) - normcdf((AC - b) / SE);
+    IMP(z) = normcdf((KAPPA - a) / SE) - normcdf((KAPPA - b) / SE);
 end
 
 %% Identify the final benchmark level using the 95% rule
