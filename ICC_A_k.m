04b08336724b3d7711b62dcf0b79bdf655ad0613
Jeffrey M Girard <j.girard@pitt.edu>
2017-12-09 10:36:00 -0600

    Correct mistakes in CI calculation
    Fix order of operations in calculation of c and fix undefined call to p in calculation of d

--- ICC_A_k.m
+++ ICC_A_k.m
@@ -16,31 +16,31 @@ function [ICC,LB,UB] = ICC_A_k(DATA,ALPHA)
 %
 %   Reference: McGraw, K. O., & Wong, S. P. (1996).
 %   Forming inferences about some intraclass correlation coefficients. 
-%   Psychological Methods, 1(1), 30–46.
+%   Psychological Methods, 1(1), 30â€“46.
 
 %% Remove any missing values
-[rowindex,~] = find(~isfinite(DATA));
-DATA(rowindex,:) = [];
+[rowindex, ~] = find(~isfinite(DATA));
+DATA(rowindex, :) = [];
 %% Calculate mean squares from two-way ANOVA
-[~,tbl,~] = anova2(DATA,1,'off');
-MSC = tbl{2,4};
-MSR = tbl{3,4};
-MSE = tbl{4,4};
+[~, tbl, ~] = anova2(DATA, 1, 'off');
+MSC = tbl{2, 4};
+MSR = tbl{3, 4};
+MSE = tbl{4, 4};
 %% Calculate average rater agreement ICC
-[n,k] = size(DATA);
-ICC = (MSR - MSE) / (MSR + (MSC - MSE)/n);
+[n, k] = size(DATA);
+ICC = (MSR - MSE) / (MSR + (MSC - MSE) / n);
 %% Calculate the confidence interval if requested
 if nargout > 1
     if nargin < 2
         ALPHA = 0.05;
     end
-    c  = ICC / (n*1 - ICC);
-    d  = 1 + (p*(n - 1)) / (n*(1 - ICC));
-    v  = ((c*MSC + d*MSE)^2) / (((c*MSC)^2)/(k - 1) + ((d*MSE)^2)/((n - 1)*(k - 1)));
-    FL = finv((1 - ALPHA/2),(n - 1),v);
-    FU = finv((1 - ALPHA/2),v,(n - 1));
-    LB = (n*(MSR - FL*MSE)) / (FL*(MSC - MSE) + n*MSR);
-    UB = (n*(FU*MSR - MSE)) / (MSC - MSE + n*FU*MSR);
+    c  = ICC / (n * (1 - ICC));
+    d  = 1 + (ICC * (n - 1)) / (n * (1 - ICC));
+    v  = ((c * MSC + d * MSE) ^ 2) / (((c * MSC) ^ 2) / (k - 1) + ((d * MSE) ^ 2) / ((n - 1) * (k - 1)));
+    FL = finv((1 - ALPHA / 2), (n - 1), v);
+    FU = finv((1 - ALPHA / 2), v, (n - 1));
+    LB = (n * (MSR - FL * MSE)) / (FL * (MSC - MSE) + n * MSR);
+    UB = (n * (FU * MSR - MSE)) / (MSC - MSE + n * FU * MSR);
 end
 
 end
