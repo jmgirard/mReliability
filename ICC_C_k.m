04031ac5dcb292371dda4f0cf1612ad19819403e
Jeffrey M Girard <j.girard@pitt.edu>
2017-12-09 10:49:37 -0600

    Clean code and verify CI calculation

--- ICC_C_k.m
+++ ICC_C_k.m
@@ -1,6 +1,6 @@
-function [ICC,LB,UB] = ICC_C_k(DATA,ALPHA)
+function [ICC, LB, UB] = ICC_C_k(DATA, ALPHA)
 % Calculate the average rater consistency intraclass correlation coefficient
-%   [ICC,LB,UB] = ICC_C_k(DATA)
+%   [ICC, LB, UB] = ICC_C_k(DATA)
 %
 %   DATA is a numerical matrix of ratings (missing values = NaN).
 %   Each row is a single item and each column is a single rater.
@@ -16,27 +16,27 @@ function [ICC,LB,UB] = ICC_C_k(DATA,ALPHA)
 %
 %   Reference: McGraw, K. O., & Wong, S. P. (1996).
 %   Forming inferences about some intraclass correlation coefficients. 
-%   Psychological Methods, 1(1), 30–46.
+%   Psychological Methods, 1(1), 30â€“46.
 
 %% Remove any missing values
-[rowindex,~] = find(~isfinite(DATA));
-DATA(rowindex,:) = [];
+[rowindex, ~] = find(~isfinite(DATA));
+DATA(rowindex, :) = [];
 %% Calculate mean squares from two-way ANOVA
-[~,tbl,~] = anova2(DATA,1,'off');
-MSR = tbl{3,4};
-MSE = tbl{4,4};
+[~, tbl, ~] = anova2(DATA, 1, 'off');
+MSR = tbl{3, 4};
+MSE = tbl{4, 4};
 %% Calculate average rater consistency ICC
-[n,k] = size(DATA);
+[n, k] = size(DATA);
 ICC = (MSR - MSE) / MSR;
 %% Calculate the confidence interval if requested
 if nargout > 1
     if nargin < 2
         ALPHA = 0.05;
     end
-    FL = (MSR/MSE) / finv((1-ALPHA/2),(n - 1),(n - 1)*(k - 1));
-    FU = (MSR/MSE) / finv((1-ALPHA/2),(n - 1)*(k - 1),(n - 1));
-    LB = 1 - 1/FL;
-    UB = 1 - 1/FU;
+    FL = (MSR / MSE) / finv((1 - ALPHA / 2), (n - 1), (n - 1) * (k - 1));
+    FU = (MSR / MSE) / finv((1 - ALPHA / 2), (n - 1) * (k - 1), (n - 1));
+    LB = 1 - 1 / FL;
+    UB = 1 - 1 / FU;
 end
 
 end
