994ac83213ea321aea9778cc9c50b1629c56b2c4
Jeffrey M Girard <j.girard@pitt.edu>
2017-12-09 10:43:35 -0600

    Clean code and verify CI formula

--- ICC_A_1.m
+++ ICC_A_1.m
@@ -1,4 +1,4 @@
-function [ICC,LB,UB] = ICC_A_1(DATA,ALPHA)
+function [ICC, LB, UB] = ICC_A_1(DATA, ALPHA)
 % Calculate the single rater agreement intraclass correlation coefficient
 %   [ICC,LB,UB] = ICC_A_1(DATA)
 %
@@ -16,31 +16,31 @@ function [ICC,LB,UB] = ICC_A_1(DATA,ALPHA)
 %
 %   Reference: McGraw, K. O., & Wong, S. P. (1996).
 %   Forming inferences about some intraclass correlation coefficients. 
-%   Psychological Methods, 1(1), 30–46.
+%   Psychological Methods, 1(1), 30â€“46.
 
 %% Remove any missing values
-[rowindex,~] = find(~isfinite(DATA));
-DATA(rowindex,:) = [];
+[rowindex, ~] = find(~isfinite(DATA));
+DATA(rowindex, :) = [];
 %% Calculate mean squares from two-way ANOVA
-[~,tbl,~] = anova2(DATA,1,'off');
-MSC = tbl{2,4};
-MSR = tbl{3,4};
-MSE = tbl{4,4};
+[~, tbl, ~] = anova2(DATA, 1, 'off');
+MSC = tbl{2, 4};
+MSR = tbl{3, 4};
+MSE = tbl{4, 4};
 %% Calculate single rater agreement ICC
-[n,k] = size(DATA);
-ICC = (MSR - MSE) / (MSR + MSE*(k - 1) + (k/n)*(MSC - MSE));
+[n, k] = size(DATA);
+ICC = (MSR - MSE) / (MSR + MSE * (k - 1) + (k / n) * (MSC - MSE));
 %% Calculate the confidence interval if requested
 if nargout > 1
     if nargin < 2
         ALPHA = 0.05;
     end
-    a  = (k*ICC) / (n*(1 - ICC));
-    b  = 1 + (k*ICC*(n - 1))/(n*(1 - ICC));
-    v  = ((a*MSC + b*MSE)^2) / (((a*MSC)^2)/(k - 1) + ((b*MSE)^2)/((n - 1)*(k - 1)));
-    FL = finv((1 - ALPHA/2),(n - 1),v);
-    FU = finv((1 - ALPHA/2),v,(n - 1));
-    LB = (n*(MSR - FL*MSE)) / (FL*(k*MSC + MSE*(k*n - k - n)) + n*MSR);
-    UB = (n*(FU*MSR - MSE)) / (k*MSC + MSE*(k*n - k - n) + n*FU*MSR);
+    a  = (k * ICC) / (n * (1 - ICC));
+    b  = 1 + (k * ICC * (n - 1)) / (n * (1 - ICC));
+    v  = ((a * MSC + b * MSE) ^ 2) / (((a * MSC) ^ 2) / (k - 1) + ((b * MSE) ^ 2) / ((n - 1) * (k - 1)));
+    FL = finv((1 - ALPHA / 2), (n - 1), v);
+    FU = finv((1 - ALPHA / 2), v, (n - 1));
+    LB = (n * (MSR - FL * MSE)) / (FL * (k * MSC + MSE * (k * n - k - n)) + n * MSR);
+    UB = (n * (FU * MSR - MSE)) / (k * MSC + MSE * (k * n - k - n) + n * FU * MSR);
 end
 
 end
